061e319251ec00cd77c25ccae0419612689b61aa
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-31 07:48:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Replit runs apps best on port 3000
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: true
-  }
-})
+    host: true, // listen on all addresses, including Replit
+    allowedHosts: [
+      ".replit.dev"
+    ],
+  },
+});
