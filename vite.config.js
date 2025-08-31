dce3697778798fdacd928efbd313c228241368da
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-08-31 08:06:15 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 3000,
-    allowedHosts: [
-      '.replit.dev',
-      '.id.repl.co', 
-      '.janeway.replit.dev'
-    ],
+    allowedHosts: ['.replit.dev', '.repl.co']
   },
-});
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['.replit.dev', '.repl.co']
+  }
+})
