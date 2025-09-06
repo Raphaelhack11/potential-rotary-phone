f12c793146ca7be89e9fbad99a42d70deeaf139a
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-09-06 17:26:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/potential-rotary-phone/'   // 👈 IMPORTANT for GitHub Pages
+  base: '/potential-rotary-phone/', // 👈 GitHub Pages base
 })
