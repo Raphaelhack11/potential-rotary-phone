9119b4f9abba7ac295ee6d142dcd7f799c19ba11
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-09-10 20:12:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from "react-router-dom";
-import ShopPage from "./pages/ShopPage";
-import PaymentPage from "./pages/PaymentPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./ProductList";
+import PaymentPage from "./PaymentPage";
+import ThankYouPage from "./ThankYouPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ShopPage />} />
-      <Route path="/payment" element={<PaymentPage />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+      </Routes>
+    </Router>
   );
 }
 
